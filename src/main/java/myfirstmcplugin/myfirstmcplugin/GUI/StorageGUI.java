4a5de8eac2fd9789290da4c6c76ee9d141615192
Liam <liamnagle5@gmail.com>
2022-08-25 14:36:00 +0100

    Added a GUI that opens when storage is right clicked.

---
+++ src/main/java/myfirstmcplugin/myfirstmcplugin/GUI/StorageGUI.java
@@ -0,0 +1,70 @@
+package myfirstmcplugin.myfirstmcplugin.GUI;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StorageGUI implements InventoryHolder {
+
+    private final Inventory inv;
+
+    public StorageGUI() {
+        inv = Bukkit.createInventory(null, 45, "Storage"); //9,18,27,36,45,54
+
+        initaliseItems();
+    }
+
+    private void initaliseItems() {
+        for(int i = 0; i < inv.getSize(); i++) {
+            inv.addItem(createGuiItem(Material.GRAY_STAINED_GLASS));
+        }
+    }
+
+    private ItemStack createGuiItem(Material material, String name, List<String> lore) {
+        ItemStack item = new ItemStack(material, 1);
+        ItemMeta meta = item.getItemMeta();
+
+        meta.setDisplayName(name);
+
+        meta.setLore(lore);
+
+        item.setItemMeta(meta);
+
+        return item;
+    }
+
+    private ItemStack createGuiItem(Material material, String name) {
+        ItemStack item = new ItemStack(material, 1);
+        ItemMeta meta = item.getItemMeta();
+
+        meta.setDisplayName(name);
+
+        item.setItemMeta(meta);
+
+        return item;
+    }
+
+    private ItemStack createGuiItem(Material material) {
+        ItemStack item = new ItemStack(material, 1);
+        ItemMeta meta = item.getItemMeta();
+
+        return item;
+    }
+
+    @Override
+    public Inventory getInventory() {
+        return inv;
+    }
+}
--- src/main/java/myfirstmcplugin/myfirstmcplugin/handlers/InventoryHandler.java
+++ src/main/java/myfirstmcplugin/myfirstmcplugin/handlers/InventoryHandler.java
@@ -1,5 +1,6 @@
 package myfirstmcplugin.myfirstmcplugin.handlers;
 
+import myfirstmcplugin.myfirstmcplugin.GUI.StorageGUI;
 import myfirstmcplugin.myfirstmcplugin.MyFirstMCPlugin;
 import org.apache.commons.lang.ArrayUtils;
 import org.bukkit.Bukkit;
@@ -9,6 +10,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class InventoryHandler implements Listener {
 
@@ -20,9 +23,28 @@ public class InventoryHandler implements Listener {
     public void onInventoryClick(InventoryClickEvent event) {
         Player player = (Player) event.getWhoClicked();
         int[] lockedSlots = {0, 9};
-        if(ArrayUtils.contains(lockedSlots, event.getRawSlot())) {
+        if(ArrayUtils.contains(lockedSlots, event.getRawSlot()) && event.isLeftClick()) {
             event.setCancelled(true);
             player.sendMessage(ChatColor.RED + "You can't move this item");
+        }
+
+        if(event.getInventory().getHolder() instanceof StorageGUI) {
+            event.setCancelled(true);
+
+            ItemStack clickedItem = event.getCurrentItem();
+
+            // verify current item is not null
+            if (clickedItem == null || clickedItem.getType().isAir()) return;
+
+            Player p = (Player) event.getWhoClicked();
+
+            // Using slots click is a best option for your inventory click's
+            p.sendMessage("You clicked at slot " + event.getRawSlot());
+        }
+
+        if(event.getCurrentItem().getItemMeta().getDisplayName() == "Storage" && event.isRightClick()); {
+            StorageGUI gui = new StorageGUI();
+            player.openInventory(gui.getInventory());
         }
     }
 
@@ -33,6 +55,10 @@ public class InventoryHandler implements Listener {
         if(event.getRawSlots().contains(lockedSlots)) {
             event.setCancelled(true);
             player.sendMessage(ChatColor.RED + "You can't move this item");
+        }
+
+        if(event.getInventory().getHolder() instanceof StorageGUI) {
+                event.setCancelled(true);
         }
     }
 }
